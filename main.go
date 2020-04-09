3cc3ca222ae8ace1b8f478fb0fc7ab958b47e24a
Travis Hampton <travis.hampton@siemens.com>
2020-04-08 22:16:27 -0500

    Array tutorial

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-const (
-	first = iota + 6
-	second
-)
-
 func main() {
-	fmt.Println(first, second)
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	fmt.Println(arr)
+	fmt.Println(arr[1])
 }
